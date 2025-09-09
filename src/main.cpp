8a17b049802c50fe31cb8fe325d34b10b2b531d7
GBasste <gabrielrodriguez775@gmail.com>
2025-09-09 20:50:49 +0000

    nvhv

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,27 @@ void ejecutarSistema();
 //Cams
 void CamConfig();
 void Camexe();
+//Motores
+void motorset();
+void cerrar_puertas();
+void abrir_puertas();
+void apagar_motor();
+void encender_motor();
+void apagar();
 
 void setup() {
   Serial.begin(115200);
   configurarSistema();
   CamConfig();
+  motorset();
 }
 
 void loop() {
   ejecutarSistema();
   Camexe();
+  cerrar_puertas();
+  abrir_puertas();
+  apagar_motor();
+  encender_motor();
+  apagar();
 }
